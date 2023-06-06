e6ed3f3e32f78913a2b6e6ed4c8df62e301c9a10
Harsh Gupta <techarshgupta@gmail.com>
2023-06-06 11:43:51 +0530

    Adding ecom page

--- components/appTabs.vue
+++ components/appTabs.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="text-center border-b border-gray-200 px-4 lg:px-0">
-    <ul class="flex flex-wrap -mb-px container mx-auto">
+    <ul class="flex-wrap -mb-px container mx-auto hidden md:flex">
       <li
         v-for="(tab, idx) in tabs"
         :key="idx"
         class="tabItem inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 cursor-pointer capitalize"
-        :class="$store.state.selectedCategory === tab ? 'active border-b-4' : 'border-transparent'"
+        :class="
+          $store.state.selectedCategory === tab
+            ? 'active border-b-4'
+            : 'border-transparent'
+        "
         @click="onClickCategory(tab)"
       >
         {{ tab }}
       </li>
     </ul>
+    <select
+      id="categories"
+      v-model="active"
+      class="md:hidden bg-gray-50 border text-sm border-gray-300 text-gray-900 rounded-lg w-full p-4 my-3 capitalize"
+      @change="onClickCategory(active)"
+    >
+      <option v-for="(tab, idx) in tabs" :key="idx"  :value="tab" class="capitalize">
+        {{ tab }}
+      </option>
+    </select>
   </div>
 </template>
 
@@ -19,33 +33,40 @@ export default {
   name: 'AppTabs',
   data() {
     return {
-      tabs: ['all','electronics','jewelery','Men\'s Clothing', 'Women\'s Clothing'],
+      active: 'all',
+      tabs: [
+        'all',
+        'electronics',
+        'jewelery',
+        "Men's Clothing",
+        "Women's Clothing",
+      ],
     }
   },
   created() {
-    this.getProductCategories();
-    this.onClickCategory('all');
+    this.getProductCategories()
+    this.onClickCategory('all')
   },
   methods: {
     async getProductCategories() {
-      const res = await this.$axios.get('products/categories');
-      this.tabs = [ 'all', ...res.data];
+      const res = await this.$axios.get('products/categories')
+      this.tabs = ['all', ...res.data]
     },
     async getProductsByCategory(category) {
-      let res;
+      let res
       if (category === 'all') {
-        res = await this.$axios.get(`products`);
+        res = await this.$axios.get(`products`)
       } else {
-        res = await this.$axios.get(`products/category/${category}`);
+        res = await this.$axios.get(`products/category/${category}`)
       }
-      return res.data;
+      return res.data
     },
     async onClickCategory(tab) {
-      const products = await this.getProductsByCategory(tab);
-      this.$store.commit('updateSelectedCategory', tab);
-      this.$store.commit('updateProducts', products);
-    }
-  }
+      const products = await this.getProductsByCategory(tab)
+      this.$store.commit('updateSelectedCategory', tab)
+      this.$store.commit('updateProducts', products)
+    },
+  },
 }
 </script>
 
